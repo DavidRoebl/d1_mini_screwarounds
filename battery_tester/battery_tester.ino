b6b7f4a4b12b298d2dd69496b39a4fa668a64d5c
roebsi <d.roebsi@gmail.com>
2022-04-19 23:08:28 +0200

    wifi ping from battery works

--- battery_tester/battery_tester.ino
+++ battery_tester/battery_tester.ino
@@ -1,9 +1,61 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include "./my_local_wifi.h"
+#include "./my_local_server.h"
 
+#define LED 5
+
+int count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("init");
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+
+  WiFi.begin(ssid, password);
+  // Connecting to WiFi...
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  // Loop continuously while WiFi is not connected
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+   
+  // Connected to WiFi
+  Serial.println();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("high");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("low");
+  delay(1000);
+  count += 1;
 
+  if(count > 10) {
+    count = 0;
+    WiFiClient client;
+    HTTPClient http;
+    
+    http.begin(client, serverName);
+    http.addHeader("Content-Type", "text/plain");
+    String message = "I'm still alive";
+    int httpResponseCode = http.POST(message);
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+
+    http.end();
+  }
 }
